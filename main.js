d4793a799416d26190cc2791e04805d952fba1f5
sebaortega <ortegamolarosebastian@gmail.com>
2022-09-04 10:03:53 -0300

    PendingFixIdpostputdelete

--- main.js
+++ main.js
@@ -3,13 +3,12 @@ const app = express()
 const port = 8080;
 const productsRouter = require('./products')
 
-app.use(expres.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use('api/mascotas', productsRouter)
+app.use('/api/products', productsRouter)
 
-
-
-
+app.use("/",)
 
 
 
--- products.js
+++ products.js
@@ -1,6 +1,4 @@
 const express = require('express')
-const app = express()
-const port = 8080
 const {Router} = express;
 
 const router = Router();
@@ -8,7 +6,7 @@ const router = Router();
 const Products = [];
 
 router.get('/', (req, res) => {
-    res.send(Products)
+    res.send({Products})
   })
 router.post('/',(res,send)=>{
     const {title, price, tumbnail} = req.body;
@@ -16,8 +14,21 @@ router.post('/',(res,send)=>{
     res.send({added:{title, price, tumbnail} })
 })
 
-
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
 exports.module = router;
+
+// Consigna: Realizar un proyecto de servidor basado en node.js y express que ofrezca una API RESTful de productos. En detalle, que incorpore las siguientes rutas:
+// GET '/api/productos' -> devuelve todos los productos.
+// GET '/api/productos/:id' -> devuelve un producto según su id.
+// POST '/api/productos' -> recibe y agrega un producto, y lo devuelve con su id asignado.
+// PUT '/api/productos/:id' -> recibe y actualiza un producto según su id.
+// DELETE '/api/productos/:id' -> elimina un producto según su id.
+// Cada producto estará representado por un objeto con el siguiente formato:
+//title, price thumnail
+// Cada ítem almacenado dispondrá de un id numérico proporcionado por el backend, comenzando en 1, y que se irá incrementando a medida de que se incorporen productos. Ese id será utilizado para identificar un producto que va a ser listado en forma individual.
+// Para el caso de que un producto no exista, se devolverá el objeto:
+// { error : 'producto no encontrado' }
+// Implementar la API en una clase separada, utilizando un array como soporte de persistencia en memoria.
+// Incorporar el Router de express en la url base '/api/productos' y configurar todas las subrutas en base a este.
+// Crear un espacio público de servidor que contenga un documento index.html con un formulario de ingreso de productos con los datos apropiados.
+// El servidor debe estar basado en express y debe implementar los mensajes de conexión al puerto 8080 y en caso de error, representar la descripción del mismo.
+// Las respuestas del servidor serán en formato JSON. La funcionalidad será probada a través de Postman y del formulario de ingreso.
